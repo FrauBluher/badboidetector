6a8dcd6eedf6cdaab26deaa7e44884677566f857
Pavlo Manovi <pavlo.manovi@gmail.com>
2018-06-27 15:23:49 -0700

    Removed xfeatures include.

--- main.cpp
+++ main.cpp
@@ -5,7 +5,6 @@
 #include <ctime>
 #include "opencv2/core.hpp"
 #include "opencv2/features2d.hpp"
-#include "opencv2/xfeatures2d.hpp"
 #include "opencv2/highgui.hpp"
 #include "opencv2/opencv.hpp"
 #include "opencv2/imgproc.hpp"
